6f965c971b29f0fd90b23c6cf0944af2ab84ea25
Abduvokhid <abduvohid1996@inbox.ru>
2020-02-22 18:51:10 +0000

    Added formulas to helper class

--- FinanceApp/HelperClasses/MortgageHelper.swift
+++ FinanceApp/HelperClasses/MortgageHelper.swift
@@ -9,5 +9,36 @@
 import Foundation
 
 class MortgageHelper {
+    static func monthlyPayment(initialValue: Double, interestRate: Double, numberOfYears: Double) -> Double{
+        let part = interestRate / 100 / 12
+        var top = pow(1 + part, 12 * numberOfYears)
+        top = initialValue * part * top
+        var bottom = pow(1 + part, 12 * numberOfYears)
+        bottom = bottom - 1
+        let calculation = top / bottom
+        return calculation
+    }
     
+    static func numberOfYears(initialValue: Double, interestRate: Double, paymentAmount: Double) -> Double{
+        let part = interestRate / 100
+        var top = initialValue * part
+        top = top - (12 * paymentAmount)
+        top = (-12 * paymentAmount) / top
+        top = log(top)
+        var bottom = (part + 12) / 12
+        bottom = 12 * log(bottom)
+        let calculation = top / bottom
+        return calculation
+    }
+    
+    static func initialValue(paymentAmount: Double, interestRate: Double, numberOfYears: Double) -> Double{
+        let part = interestRate / 100 / 12
+        var top = pow(1 + part, 12 * numberOfYears)
+        top = top - 1
+        top = paymentAmount * top
+        var bottom = pow(1 + part, 12 * numberOfYears)
+        bottom = part * bottom
+        let calculation = top / bottom
+        return calculation
+    }
 }
---
+++ FinanceApp/MortgageViewController.swift
@@ -0,0 +1,65 @@
+//
+//  MortgageViewController.swift
+//  FinanceApp
+//
+//  Created by Abduvokhid Akhmedov on 22/02/2020.
+//  Copyright © 2020 Abduvokhid Akhmedov. All rights reserved.
+//
+
+import UIKit
+
+enum MortgageFinding {
+    case empty
+    case initialAmount
+    case paymentAmount
+    case numberOfYears
+}
+
+class MortgageViewController: UIViewController {
+
+    @IBOutlet weak var initialAmountTF: UITextField!
+    @IBOutlet weak var paymentAmountTF: UITextField!
+    @IBOutlet weak var numberOfYearsTF: UITextField!
+    @IBOutlet weak var interestRateTF: UITextField!
+    
+    var finding = MortgageFinding.empty
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        var counter = 0
+        
+        let interestRate: Double! = Double(interestRateTF.text!)
+        if (interestRate == nil) {
+            return
+        }
+        
+        let initialAmount: Double! = Double(initialAmountTF.text!)
+        if (initialAmount == nil) {
+            counter += 1
+            finding = .initialAmount
+        }
+        
+        let paymentAmount: Double! = Double(paymentAmountTF.text!)
+        if (paymentAmount == nil) {
+            counter += 1
+            finding = .paymentAmount
+        }
+        
+        let numberOfYears: Double! = Double(numberOfYearsTF.text!)
+        if (numberOfYears == nil) {
+            counter += 1
+            finding = .numberOfYears
+        }
+        
+        if ((counter == 0 && finding == .empty) || counter > 1) {
+            finding = .empty
+            return
+        }
+        
+    }
+    
+    
+}
