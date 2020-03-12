32ec4bd5f502024af1605ee9661e03fe2ae03f60
Abduvokhid <Abduvohid1996@inbox.ru>
2020-03-12 21:15:14 +0000

    Added formula to Help view

--- FinanceApp/Pages/HelpPageViewController.swift
+++ FinanceApp/Pages/HelpPageViewController.swift
@@ -17,7 +17,7 @@ class HelpPageViewController: UIViewController {
     
     var titleText: String = ""
     var helpText: String = ""
-    var helpAttributes: [AttributeInfo] = []
+    var helpFormula: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,31 +31,7 @@ class HelpPageViewController: UIViewController {
         
         titleLabel.text = titleText
         
-        let commonAttribute: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14, weight: .regular)
-        ]
-        
-        let boldAttribute: [NSAttributedString.Key: Any] = [
-            .font: UIFont.systemFont(ofSize: 14, weight: .semibold)]
-        
-        let newText: NSMutableAttributedString = NSMutableAttributedString(attributedString: helpText.htmlAttributed(family: "Arial", size: 12, color: "2c3e50")!)
-        //let newText: NSMutableAttributedString = NSMutableAttributedString(attributedString: NSAttributedString(string: helpText))
-        
-        //newText.addAttributes(commonAttribute, range: NSRange(location: 0, length: helpText.count))
-        
-        for helpAttribute in helpAttributes {
-            let attribute: [NSAttributedString.Key: Any]
-            switch helpAttribute.type {
-            case .Bold:
-                attribute = boldAttribute
-            default:
-                attribute = commonAttribute
-            }
-            newText.addAttributes(attribute, range: NSRange(location: helpAttribute.start, length: helpAttribute.length))
-        }
-        
-        
-        helpTextView.attributedText = newText
+        helpTextView.attributedText = helpText.htmlAttributed(family: "Arial", size: 12, color: "2c3e50")!
     }
     
     @IBAction func dismissSecondVC(_ sender: AnyObject) {
