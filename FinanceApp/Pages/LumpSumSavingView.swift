91ff939c011918c9d98e33ac4a4d969cb5d40a47
Abduvokhid <Abduvohid1996@inbox.ru>
2020-03-15 16:25:13 +0000

    Finalising

--- FinanceApp/Pages/LumpSumSavingView.swift
+++ FinanceApp/Pages/LumpSumSavingView.swift
@@ -1,5 +1,5 @@
 //
-//  LumpSumView.swift
+//  LumpSumSavingView.swift
 //  FinanceApp
 //
 //  Created by Abduvokhid Akhmedov on 07/03/2020.
@@ -13,7 +13,7 @@ import UIKit
 // Comments were written only for complex snippets of code as it is mentioned in Coursework description
 //
 
-class LumpSumView: UIView, UITextFieldDelegate, Slide {
+class LumpSumSavingView: UIView, UITextFieldDelegate, Slide {
     
     enum CompoundFinding{
         case Empty
